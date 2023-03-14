ddad8bbb8429924942dbc4da3a210a0b40b78f3a
DsvHitSystems <106150778+aguilator69@users.noreply.github.com>
2023-03-14 17:45:38 +0100

    promociones.clase actualizado
    se ha integrado una nueva condición donde se calcula la Const diferencia que modificará uno de los dos precios dependiendo cual de estos dos requiere mas cantidad.

--- src/promociones/promociones.clase.ts
+++ src/promociones/promociones.clase.ts
@@ -707,7 +707,15 @@ export class NuevaPromocion {
       });
     }
   }
-
+  private redondearDecimales(numero, decimales) {
+    let numeroRegexp = new RegExp("\\d\\.(\\d){" + decimales + ",}"); // Expresion regular para numeros con un cierto numero de decimales o mas
+    if (numeroRegexp.test(numero)) {
+      // Ya que el numero tiene el numero de decimales requeridos o mas, se realiza el redondeo
+      return Number(numero.toFixed(decimales));
+    } else {
+      return Number(numero.toFixed(decimales)) === 0 ? 0 : numero; // En valores muy bajos, se comprueba si el numero es 0 (con el redondeo deseado), si no lo es se devuelve el numero otra vez.
+    }
+  }
   calcularPrecioRealCombo(
     data: InfoPromocionCombo,
     articuloPrincipal: ArticulosInterface,
@@ -773,15 +781,24 @@ export class NuevaPromocion {
     };
 
     if (
-      devolver.precioRealPrincipal * data.cantidadNecesariaPrincipal +
-        devolver.precioRealSecundario * data.cantidadNecesariaSecundario !==
-      data.precioPromoUnitario
-    ) {
-      const diferencia =
+      this.redondearDecimales(
         devolver.precioRealPrincipal * data.cantidadNecesariaPrincipal +
-        devolver.precioRealSecundario * data.cantidadNecesariaSecundario -
-        data.precioPromoUnitario;
-      devolver.precioRealPrincipal += diferencia * -1;
+          devolver.precioRealSecundario * data.cantidadNecesariaSecundario,
+        2
+      ) !== data.precioPromoUnitario
+    ) {
+      const diferencia = this.redondearDecimales(
+        devolver.precioRealPrincipal * data.cantidadNecesariaPrincipal +
+          devolver.precioRealSecundario * data.cantidadNecesariaSecundario -
+          data.precioPromoUnitario,
+        2
+      );
+
+      if (data.cantidadNecesariaPrincipal < data.cantidadNecesariaSecundario) {
+        devolver.precioRealPrincipal += diferencia * -1;
+      } else {
+        devolver.precioRealSecundario += diferencia * -1;
+      }
     }
     return devolver;
   }
