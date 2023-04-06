c1f29211844b7f64d71c90f0dacbaba8f343b8a7
eniorJR <marquesnooriok@gmail.com>
2023-04-06 12:35:24 +0200

    mesas abajo cestas

--- src/cestas/cestas.mongodb.ts
+++ src/cestas/cestas.mongodb.ts
@@ -25,7 +25,7 @@ export async function deleteCesta(
 export async function getAllCestas(): Promise<CestasInterface[]> {
   const database = (await conexion).db("tocgame");
   const cesta = database.collection<CestasInterface>("cestas");
-  return await cesta.find().toArray();
+  return await cesta.find().sort( { indexMesa: 1 } ).toArray();
 }
 
 /* Eze 4.0 */
