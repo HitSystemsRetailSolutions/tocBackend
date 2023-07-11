7a3a679e85d4460727d6700a5c0262dd88205337
Alejandro <alexvidalcasado@gmail.com>
2023-07-11 16:17:00 +0200

    tarea infernal va

--- src/imagenInvasiva/imagen.controller.ts
+++ src/imagenInvasiva/imagen.controller.ts
@@ -1,23 +1,29 @@
-import { Controller, Post, Body } from "@nestjs/common";
+import { Controller, Post, Body, Query, Param } from "@nestjs/common";
 import { io } from "../sockets.gateway";
+import { parametrosController } from "src/parametros/parametros.controller";
 const mqtt = require("mqtt");
-const client = mqtt.connect("mqtt://localhost:6969");
+const client = mqtt.connect("mqtt://localhost:1883");
 
-client.on("connect", () => {
-  client.subscribe("hit.software/imagen");
+client.on("connect", async () => {
+  const parametros = await parametrosController.getParametros();
+  client.subscribe(`hit.software/imagen/${parametros.licencia}`);
 });
 
 client.on("message", (topic, message) => {
-  if (topic == "hit.software/imagen") {
-    io.emit("ponerImagen", message.toString());
+  if (topic.includes("hit.software/imagen")) {
+    const mensaje = Buffer.from(message, "binary").toString("utf-8");
+    io.emit("ponerImagen", JSON.parse(mensaje));
   }
 });
 
-@Controller("/cargarImagen")
+@Controller("/cargarImagen/:licencia")
 export class CargarImagenController {
   @Post()
-  async cargarImagen(@Body() { imagen }) {
-    io.emit("ponerImagen", imagen);
+  async cargarImagen(@Param("licencia") license, @Body() datos) {
+    const parametros = await parametrosController.getParametros();
+    if (license != parametros.licencia) return false;
+
+    io.emit("ponerImagen", datos);
     return true;
   }
 }
