7e401a7eee433c654f59a3549c216872e389da46
Alejandro <alexvidalcasado@gmail.com>
2023-07-11 13:30:01 +0200

    creo que ahora tiene mqtt (creo)

--- src/imagenInvasiva/imagen.controller.ts
+++ src/imagenInvasiva/imagen.controller.ts
@@ -1,5 +1,17 @@
 import { Controller, Post, Body } from "@nestjs/common";
 import { io } from "../sockets.gateway";
+const mqtt = require("mqtt");
+const client = mqtt.connect("mqtt://localhost:6969");
+
+client.on("connect", () => {
+  client.subscribe("hit.software/imagen");
+});
+
+client.on("message", (topic, message) => {
+  if (topic == "hit.software/imagen") {
+    io.emit("ponerImagen", message.toString());
+  }
+});
 
 @Controller("/cargarImagen")
 export class CargarImagenController {
