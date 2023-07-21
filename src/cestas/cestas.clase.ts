8c9e333763af60cb93923e7ab90f6e1d2825cc3d
eniorJR <marquesnooriok@gmail.com>
2023-07-21 17:33:49 +0200

    tusabelomeguta, te boy a cobrar la multa

--- src/cestas/cestas.clase.ts
+++ src/cestas/cestas.clase.ts
@@ -140,6 +140,7 @@ export class CestaClase {
           id,
           e.unidades,
           e.arraySuplementos,
+          "",
           ""
         );
       }
@@ -156,6 +157,7 @@ export class CestaClase {
         cesta,
         e.unidades,
         e.arraySuplementos,
+        "",
         ""
       );
     }
@@ -179,10 +181,7 @@ export class CestaClase {
       let cesta = await this.getCestaById(idCesta);
       let productos = [];
       productos.push(cesta.lista[index]);
-      await this.registroLogSantaAna(
-        cesta,
-        productos
-      );
+      await this.registroLogSantaAna(cesta, productos);
 
       cesta.lista.splice(index, 1);
       // Enviar por socket
@@ -303,7 +302,8 @@ export class CestaClase {
     idCesta: CestasInterface["_id"],
     arraySuplementos: ItemLista["arraySuplementos"], // Los suplentos no deben tener tarifa especial para simplificar.
     gramos: ItemLista["gramos"],
-    nombre = ""
+    nombre = "",
+    menu = ""
   ): Promise<CestasInterface> {
     // recojemos la cesta
     let cesta = await this.getCestaById(idCesta);
@@ -316,9 +316,10 @@ export class CestaClase {
         unidades
       ))
     ) {
+      articulo["menu"] = menu;
       // recojemos los datos del articulo
       let infoArticulo = await articulosInstance.getInfoArticulo(articulo._id);
-      // recorremos la cesta 
+      // recorremos la cesta
       for (let i = 0; i < cesta.lista.length; i++) {
         // si el articulo ya esta en la cesta
         if (
@@ -373,7 +374,6 @@ export class CestaClase {
             articuloNuevo = false;
             break;
           }
-
         }
       }
 
@@ -404,7 +404,7 @@ export class CestaClase {
       });
     }
 
-    await this.recalcularIvas(cesta);
+    await this.recalcularIvas(cesta, menu);
 
     if (await schCestas.updateCesta(cesta)) return cesta;
 
@@ -418,7 +418,8 @@ export class CestaClase {
     idCesta: CestasInterface["_id"],
     unidades: number,
     arraySuplementos: ItemLista["arraySuplementos"],
-    nombre: string
+    nombre: string,
+    menu: string
   ) {
     if (await cajaInstance.cajaAbierta()) {
       let articulo = await articulosInstance.getInfoArticulo(idArticulo);
@@ -441,7 +442,8 @@ export class CestaClase {
           idCesta,
           arraySuplementos,
           gramos,
-          nombre
+          nombre,
+          menu
         );
 
       // Modo por unidad
@@ -451,7 +453,8 @@ export class CestaClase {
         idCesta,
         arraySuplementos,
         null,
-        nombre
+        nombre,
+        menu
       );
     }
     throw Error(
@@ -536,7 +539,7 @@ export class CestaClase {
   }
 
   /* Eze 4.0 */
-  async recalcularIvas(cesta: CestasInterface) {
+  async recalcularIvas(cesta: CestasInterface, menu: string = "") {
     cesta.detalleIva = {
       base1: 0,
       base2: 0,
@@ -577,6 +580,14 @@ export class CestaClase {
               ? articulo.precioConIva
               : (await tarifasInstance.tarifaMesas(cesta.lista[i].idArticulo))
                   .precioConIva;
+        }
+        if (menu.length > 0) {
+          let preu = await tarifasInstance.tarifaMenu(
+            cesta.lista[i].idArticulo,
+            menu
+          );
+          articulo.precioConIva =
+            preu == null ? articulo.precioConIva : preu.precioConIva;
         }
 
         const auxDetalleIva = construirObjetoIvas(
@@ -786,9 +797,9 @@ export class CestaClase {
     cesta: CestasInterface,
     productos: CestasInterface["lista"]
   ) {
-
     let cliente: number =
-      (await clienteInstance.getClienteById(cesta.idCliente))?.descuento == undefined
+      (await clienteInstance.getClienteById(cesta.idCliente))?.descuento ==
+      undefined
         ? 0
         : Number(
             (await clienteInstance.getClienteById(cesta.idCliente))?.descuento
@@ -806,9 +817,13 @@ export class CestaClase {
       idCliente: cesta.idCliente,
     };
 
-    await axios.post("lista/setRegistro", {
-      lista: lista,
-    }).catch((err) => {console.error("Error al enviar el registro a Santa Ana");});
+    await axios
+      .post("lista/setRegistro", {
+        lista: lista,
+      })
+      .catch((err) => {
+        console.error("Error al enviar el registro a Santa Ana");
+      });
   }
 }
 
