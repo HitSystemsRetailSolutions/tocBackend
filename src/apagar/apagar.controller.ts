a9f9df03cec048f9897a8f9c114bb718e91c7d56
eniorJR <marquesnooriok@gmail.com>
2024-03-11 17:55:30 +0100

    save-interno-nofinalizado-#1

--- src/apagar/apagar.controller.ts
+++ src/apagar/apagar.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Post } from "@nestjs/common";
+import { Controller, Get, Post, Req, Res } from "@nestjs/common";
 import { apagarinstance } from "./apagar.class";
 
 @Controller("controlTpv")
 export class ApagarController {
   @Post("ApagarOrdenador")
-  apagar() {
+  apagar(@Req() req: Request, @Res() res: Response) {
+    //console.log(JSON.parse(req.headers["authorization"]).id);
     return apagarinstance.apagarEquipo();
   }
 }
