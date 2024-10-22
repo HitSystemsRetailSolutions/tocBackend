13595babdc09644d56ff48e199d33949d832e2e6
sheilamr <sheila.24.1996@gmail.com>
2024-10-22 11:44:03 +0200

    3002

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ require("./sockets.gateway");
 axios.defaults.baseURL =
   process.env.npm_lifecycle_event === "start:dev"
     ? "http://localhost:3001"
-    : "https://santaana2-elb.nubehit.com:3003";
+    : "https://santaana2-elb.nubehit.com:3002";
 
 parametrosInstance
   .getParametros()
